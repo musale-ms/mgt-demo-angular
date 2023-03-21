e7c8485e62aef2d16ab2ec9181e41059c9d43e22
Martin Musale <martinmusale@microsoft.com>
2023-03-21 16:16:35 +0300

    feat: add get component
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AgendaComponent } from './agenda/agenda.component';
 import { ToDoComponent } from './to-do/to-do.component';
+import { GetComponent } from './get/get.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     AgendaComponent,
-    ToDoComponent
+    ToDoComponent,
+    GetComponent
   ],
   imports: [
     BrowserModule,
