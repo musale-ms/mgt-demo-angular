10be80ffcf19731b56f339c557606156a28dc069
Martin Musale <martinmusale@microsoft.com>
2023-03-21 16:10:26 +0300

    feat: add todo component with no-data tpl
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AgendaComponent } from './agenda/agenda.component';
+import { ToDoComponent } from './to-do/to-do.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    AgendaComponent
+    AgendaComponent,
+    ToDoComponent
   ],
   imports: [
     BrowserModule,
